0ff4df62d7b772a1da27166a40854d6d9c0f3318
kmooog <ryujhbv@gmail.com>
2019-02-14 17:25:26 +0900

    add megahit

--- script/metagenome-luigi.py
+++ script/metagenome-luigi.py
@@ -2,10 +2,20 @@ import luigi
 import glob 
 import subprocess
 import os
+import pathlib
+from pathlib import Path
 
 files = "*.gz"
 file_list = glob.glob(files)
 file_list = [a.replace(".fastq.gz","") for a in file_list]
+
+file_list_merged = []
+
+cwd = Path.cwd()
+
+for file_tmp in file_list:
+    if file_tmp.find("_R1_") != -1:
+        file_list_merged.append(file_tmp)
 
 script_path = "/work/G10800/kumay/metagenome-pipeline/script/"
 
@@ -42,15 +52,54 @@ class _2_Flash(luigi.Task):
     def run(self):
         print("_2_Flash: run")
         cmd = 'mkdir 02_Flash && cd 02_Flash && cp ../01_TrimGalore/*_val_*.fq.gz . && sh {0}run_FLASH.sh && cd ..'.format(script_path)
-        #subprocess.run(cmd.split())
         os.system(cmd)
     def output(self):
         print("_2_Flash: output")
         outputDict = {}
-        for i, outFile in enumerate(file_list):
-           outputDict["output" + str(i)] = luigi.LocalTarget('01_TrimGalore/' + outFile + ".extendedFrags.fastq")
+        for i, outFile in enumerate(file_list_merged):
+           outputDict["output" + str(i)] = luigi.LocalTarget('02_Flash/' + outFile + ".extendedFrags.fastq")
+           print("_2_Flash output: " + '02_Flash/' + outFile + ".extendedFrags.fastq" )
         return outputDict
 
+class _3_bowtie2(luigi.Task):
+    task_namespace = 'metagenomes'
+
+    def requires(self):
+        print("_3_bowtie2: requires")
+        return _2_Flash()
+
+    def run(self):
+        print("_3_bowtie2: run")
+        cmd = 'mkdir 03_bowtie2 && cd 03_bowtie2 && cp ../02_Flash/*.extendedFrags.fastq . && sh {0}run_bowtie2.sh && cd ..'.format(script_path)
+        os.system(cmd)
+    def output(self):
+        print("_3_bowtie2: output")
+        outputDict = {}
+        for i, outFile in enumerate(file_list_merged):
+           outputDict["output" + str(i)] = luigi.LocalTarget('03_bowtie2/' + outFile + ".nophix.fastq")
+        return outputDict
+
+class _4_megahit(luigi.Task):
+    task_namespace = 'metagenomes'
+
+    def requires(self):
+        print("_4_megahit: requires")
+        return _3_bowtie2()
+
+    def run(self):
+        print("_4_megahit: run")
+        cmd = 'mkdir 04_megahit && cd 04_megahit && cp ../03_bowtie2/*.nophix.fastq .'
+        os.system(cmd)
+        for input_file in (file_list_merged):
+           current_path = cwd/'04_megahit'
+           subprocess.Popen('echo ${FASTQ_FILE} > tmpout'.split(), stdout=subprocess.PIPE,cwd=current_path)
+           subprocess.Popen('qsub -q l {0}run_megahit.sh -v FASTQ_FILE={1},MEGAHIT_DIR={2}'.format(script_path,input_file,os.getcwd()+"/04_megahit").split(),cwd=current_path)
+    def output(self):
+        print("_4_megahit: output")
+        outputDict = {}
+        for i, outFile in enumerate(file_list_merged):
+           outputDict["output" + str(i)] = luigi.LocalTarget('04_megahit/' + outFile + ".nophixassembled/final.contigs.fa")
+        return outputDict
 
 if __name__ == '__main__':
-    luigi.run(['metagenomes._2_Flash', '--workers', '1', '--local-scheduler'])
+    luigi.run(['metagenomes._4_megahit', '--workers', '1', '--local-scheduler'])
--- script/run_megahit.sh
+++ script/run_megahit.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-cd /work/G10800/kumay/01_make_analysis_workflow/04_megahit
-megahit -r ${FASTQ_FILE} -o ${FASTQ_FILE/.fastq/assembled}
-
+cd ${MEGAHIT_DIR} 
+megahit -r ${FASTQ_FILE}.nophix.fastq -o ${FASTQ_FILE/.fastq/assembled}
+echo "MEGAHIT_DIR:$MEGAHIT_DIR"
+echo "megahit -r ${FASTQ_FILE}.nophix.fastq -o ${FASTQ_FILE/.fastq/assembled}"
 
 
