103513e7bcbbe3c471cd6f5d229e175134cf2171
Yohei Kumagai <ryujhbv@gmail.com>
2019-04-18 17:59:58 +0900

    some mod

--- script/run_bowtie2.sh
+++ script/run_bowtie2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for file in `ls *extendedFrags.fastq`; do
-     bowtie2 -x /work/G10800/kumay/metagenome-pipeline/util-data/phix ${file} --un ./${file/extendedFrags.fastq/nophix.fastq} -S ./${file/extendedFrags.fastq/log}
+     bowtie2 -x /root/metagenome-pipeline/util-data/phix ${file} --un ./${file/extendedFrags.fastq/nophix.fastq} -S ./${file/extendedFrags.fastq/log}
 done
 
 
